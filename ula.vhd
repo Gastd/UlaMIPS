e8277843d9324a5a1955510c20142abd4da4ea44
Gastd <gabriel.araujo.5000@gmail.com>
2018-05-30 10:15:15 -0300

    Remove unnecessary variable and operations

--- ula.vhd
+++ ula.vhd
@@ -22,10 +22,9 @@ begin
     tmp2 <= std_logic_vector(unsigned(A) - unsigned(B));
     Z <= result;
     proc_ula: process (A, B, aluctl, result, tmp, tmp2) is
-        variable shift : integer;
     begin
-        if (result = X"00000000") then zero <= '1'; else zero <= '0'; end if;
         ovfl <= '0';
+        if (result = X"00000000") then zero <= '1'; else zero <= '0'; end if;
         case aluctl is
             when  AND_OP =>
                 result <= A and B; -- and
@@ -50,25 +49,15 @@ begin
             when  XOR_OP =>
                 result <= A xor B; -- xor
             when  SLL_OP =>
-                shift := to_integer(unsigned(B));
-                shift := to_integer(shift_left(to_unsigned(shift, 32), to_integer(unsigned(A)))); -- sll
-                result <= std_logic_vector(to_unsigned(shift, 32));
+                result <= std_logic_vector(shift_left(unsigned(B), to_integer(unsigned(A)))); -- sll
             when  SRL_OP =>
-                shift := to_integer(unsigned(B));
-                shift := to_integer(shift_right(to_unsigned(shift, 32), to_integer(unsigned(A)))); -- srl
-                result <= std_logic_vector(to_unsigned(shift, 32));
+                result <= std_logic_vector(shift_right(unsigned(B), to_integer(unsigned(A)))); -- srl
             when  SRA_OP =>
-                shift := to_integer(unsigned(B));
-                shift := to_integer(shift_right(to_signed(shift, 32), to_integer(unsigned(A)))); -- sra
-                result <= std_logic_vector(to_signed(shift, 32));
+                result <= std_logic_vector(shift_right(signed(B), to_integer(unsigned(A)))); -- sra
             when  RTR_OP =>
-                shift := to_integer(unsigned(B));
-                shift := to_integer(rotate_right(to_signed(shift, 32), to_integer(unsigned(A)))); -- rtr
-                result <= std_logic_vector(to_signed(shift, 32));
+                result <= std_logic_vector(rotate_right(unsigned(B), to_integer(unsigned(A)))); -- rtr
             when  RTL_OP =>
-                shift := to_integer(unsigned(B));
-                shift := to_integer(rotate_left(to_signed(shift, 32), to_integer(unsigned(A)))); -- rtl
-                result <= std_logic_vector(to_signed(shift, 32));
+                result <= std_logic_vector(rotate_left(unsigned(B), to_integer(unsigned(A)))); -- rtl
             when others  =>
                 result <= (others => '0');
         end case;
